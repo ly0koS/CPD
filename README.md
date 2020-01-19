4f220be98061e5c7fc28f8774e9a9edc2f8ef754
Jeremy shen <ly0koS@hotmail.com>
2020-01-19 19:30:25 +0800

    Add genplate from github.com/szad670401/end-to-end-for-chinese-plate-recognition

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CPD
 Car Plate Detection
+Plate Generator from https://github.com/szad670401/end-to-end-for-chinese-plate-recognition
--- ccpd_forward.py
+++ ccpd_forward.py
@@ -6,7 +6,6 @@ from keras import layers as layers
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras.optimizers import SGD
 
 INPUT_NODE = 835200
 
@@ -29,12 +28,12 @@ def ReadData(path):
                             labels.append(name)
                             num+=1
        labels=np.array(labels)
-       data=np.zeros((num,480,480,1))
+       data=np.zeros((num,72,272,3))
        while i<len(labels):
               dir=path+str(labels[i])
               image=tf.io.read_file(dir)
               image=tf.image.decode_jpeg(image)
-              image=tf.cast(image,tf.float64)/255.0
+              image=tf.cast(image,tf.float32)/255.0
               data[i]=image
               i+=1
        shape=labels.shape[0]
@@ -43,11 +42,13 @@ def ReadData(path):
 
 def Forward(files):
        model=Sequential()
-       model.add(Conv2D(24,(3,3),activation="relu",padding='same',data_format="channels_last",input_shape=(480,480,1)))
+       model.add(Conv2D(24,(3,3),activation="relu",padding='same',input_shape=(72,272,3),use_bias=True,kernel_initializer="he_normal"))
        model.add(MaxPooling2D((2,2),strides=2))
-       model.add(Conv2D(48,(3,3),activation="relu",padding='same'))
+       model.add(Conv2D(48,(3,3),activation="relu",padding='same',kernel_initializer="he_normal"))
        model.add(MaxPooling2D((2,2),strides=2))
-       model.add(Conv2D(48,(3,3),activation="relu",padding='same'))
+       model.add(Conv2D(96,(3,3),activation="relu",padding='same',kernel_initializer="he_normal"))
+       model.add(MaxPooling2D((2,2),strides=2))
+       model.add(Conv2D(128,(3,3),activation="relu",padding='same',kernel_initializer="he_normal"))
        model.add(layers.Flatten())
        model.add(layers.Dense(48, activation='relu'))
        model.add(layers.Dense(10, activation='softmax'))
@@ -61,9 +62,8 @@ def Forward(files):
        return model
 
 def main():
-       test_data=ReadData("output/")
-       train_data,train_lables=ReadData("train/")
-
+       test_data=ReadData("plate/")
+       train_data,train_lables=ReadData("plate_test/")
        model=Forward(test_data)
        model.fit(train_data,train_lables,epochs=10)
        
