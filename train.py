93201a8022d38540a99d8daaea661294be61cce3
Jeremy shen <ly0koS@hotmail.com>
2020-03-04 20:09:13 +0800

    Add repeat & prefetch

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ from Genplate import *
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 PATH = "/home/ly0kos/Car/"
 SAVE_PATH="/home/ly0kos/Car/model/"
-BATCH_SIZE = 64
+BATCH_SIZE = 20
 
 def PlateData(count, height, width):
     genplate=GenPlate("/home/ly0kos/Car/font/platech.ttf",'/home/ly0kos/Car/font/platechar.ttf')
@@ -27,6 +27,7 @@ def PlateData(count, height, width):
     labelCh6=np.empty((count,1,1))
     data,label=genplate.genBatch(count,"/home/ly0kos/Car/temp",(height,width))
     data=np.asarray(data)
+    data=data/255.0
     for i in range(0,count):
         labelZh[i]=label[i][0]
         labelCh1[i]=label[i][1]
@@ -38,17 +39,19 @@ def PlateData(count, height, width):
     dataset=tf.data.Dataset.from_tensor_slices((
         data,
         {
-            'dense': labelZh,
-            'dense_1': labelCh1,
-            'dense_2': labelCh2,
-            'dense_3': labelCh3,
-            'dense_4': labelCh4,
-            'dense_5': labelCh5,
-            'dense_6': labelCh6
+            'dense_1': labelZh,
+            'dense_2': labelCh1,
+            'dense_3': labelCh2,
+            'dense_4': labelCh3,
+            'dense_5': labelCh4,
+            'dense_6': labelCh5,
+            'dense_7': labelCh6
         }
         ))
     dataset=dataset.shuffle(buffer_size=1000)
+    dataset=dataset.repeat()
     dataset=dataset.batch(BATCH_SIZE)
+    dataset=dataset.prefetch(buffer_size=AUTOTUNE)
     return dataset    
         
 
@@ -63,31 +66,33 @@ def Forward():
     x=layers.MaxPooling2D(2)(x)
     x=layers.Conv2D(64,3,activation="relu",padding='same',kernel_initializer="he_normal")(x)
     x=layers.MaxPooling2D(2)(x)
-    x=layers.Conv2D(64,3,activation="relu",padding='same',kernel_initializer="he_normal")(x)
+    x=layers.Conv2D(128,3,activation="relu",padding='same',kernel_initializer="he_normal")(x)
     x=layers.MaxPooling2D(2)(x)
-    x=layers.Conv2D(64,3,activation="relu",padding='same',kernel_initializer="he_normal")(x)
+    x=layers.Conv2D(256,3,activation="relu",padding='same',kernel_initializer="he_normal")(x)
     x=layers.MaxPooling2D(2)(x)
     x=layers.Dropout(0.20)(x)
     x=layers.Flatten()(x)
-    output_Zh=layers.Dense(65)(x)
-    output_1=layers.Dense(65)(x)
-    output_2=layers.Dense(65)(x)
-    output_3=layers.Dense(65)(x)
-    output_4=layers.Dense(65)(x)
-    output_5=layers.Dense(65)(x)
-    output_6=layers.Dense(65)(x)
+    x=layers.Dense(120,activation="relu")(x)
+    output_Zh=layers.Dense(65,activation="softmax")(x)
+    output_1=layers.Dense(65,activation="softmax")(x)
+    output_2=layers.Dense(65,activation="softmax")(x)
+    output_3=layers.Dense(65,activation="softmax")(x)
+    output_4=layers.Dense(65,activation="softmax")(x)
+    output_5=layers.Dense(65,activation="softmax")(x)
+    output_6=layers.Dense(65,activation="softmax")(x)
     model=keras.Model(inputs=input,outputs=[output_Zh,output_1,output_2,output_3,output_4,output_5,output_6])
     
     return model
 
-dataset=PlateData(500000,64,64)
+
+dataset=PlateData(10000,64,64)
 
 model=Forward()
 model.compile(optimizer='adam',
-              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True,name="loss"),
+              loss=keras.losses.SparseCategoricalCrossentropy(from_logits=False,name="loss"),
               metrics=["accuracy"]) 
-
-model.fit(dataset,epochs=500)
+steps=tf.math.ceil(10000/BATCH_SIZE).numpy()
+model.fit(dataset,steps_per_epoch=steps,epochs=20)
 
 #save_model=os.path.join(SAVE_PATH,"Han/")
 
